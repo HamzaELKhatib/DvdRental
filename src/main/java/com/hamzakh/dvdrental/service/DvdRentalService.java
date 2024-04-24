a57229b22f751d3b361a3af5e5529fdabe0f6a70
HamzaELKhatib <contact@hamzaelkhatib.com>
2024-04-24 01:33:16 +0100

    feat: Adding new entities and service cleanup

--- src/main/java/com/hamzakh/dvdrental/service/DvdRentalService.java
+++ src/main/java/com/hamzakh/dvdrental/service/DvdRentalService.java
@@ -2,12 +2,20 @@ package com.hamzakh.dvdrental.service;
 
 import com.hamzakh.dvdrental.entity.*;
 import com.hamzakh.dvdrental.repository.*;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StopWatch;
 
+import java.sql.Timestamp;
 import java.util.*;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
+@Slf4j
 @Service
 public class DvdRentalService {
+
+    Logger logger = Logger.getLogger(DvdRentalService.class.getName());
 
     final CountryRepository categoryRepository;
 
@@ -93,16 +101,49 @@ public class DvdRentalService {
     }
 
     public String getWelcomeMessage() {
+        StopWatch stopWatch = new StopWatch();
+
+        stopWatch.start("countries");
         List<Country> countries = this.getCountries();
+        stopWatch.stop();
+
+        stopWatch.start("payments");
         List<Payment> payments = this.getPayments();
+        stopWatch.stop();
+
+        stopWatch.start("films");
         List<Film> films = this.getFilms();
+        stopWatch.stop();
+
+        stopWatch.start("addresses");
         List<Address> addresses = this.getAddresses();
+        stopWatch.stop();
+
+        stopWatch.start("cities");
         List<City> cities = this.getCities();
+        stopWatch.stop();
+
+        stopWatch.start("customers");
         List<Customer> customers = this.getCustomers();
+        stopWatch.stop();
+
+        stopWatch.start("inventories");
         List<Inventory> inventories = this.getInventories();
+        stopWatch.stop();
+
+        stopWatch.start("rentals");
         List<Rental> rentals = this.getRentals();
+        stopWatch.stop();
+
+        stopWatch.start("staffs");
         List<Staff> staffs = this.getStaffs();
+        stopWatch.stop();
+
+        stopWatch.start("stores");
         List<Store> stores = this.getStores();
+        stopWatch.stop();
+
+        logger.info("Data retrieved successfully! Time taken: " + stopWatch.prettyPrint());
 
         return String.valueOf(this.getCountries().size());
     }
